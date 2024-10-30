56d8c582eee3b8b0c0d82ff679a083535bd4068f
Han-Lynn-Aung <hanlynn.aung@cdsg.com.mm>
2024-10-30 23:04:15 +0630

    added robots cards

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({id,name,email}) => {
+    return (
+        <div className="tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-4">
+            <img alt="robots" src={`https://robohash.org/${id}?size=200x200`}/>
+            <div>
+                <h>{name}</h>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Card from "./Card";
+import "tachyons";
+import reportWebVitals from "./reportWebVitals";
+import {robots} from "./robots";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+      <div>
+          {robots.map((robot) => (
+              <Card id={robot.id} name={robot.name} email={robot.email} />
+          ))}
+      </div>
   </React.StrictMode>
 );
 
