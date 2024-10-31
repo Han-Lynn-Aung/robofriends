66ed622ab675e36562bae7d454cd5d3950bd73b8
Han-Lynn-Aung <hanlynn.aung@cdsg.com.mm>
2024-10-31 23:05:39 +0630

    refactor components

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({id,name,email}) => {
         <div className="tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-4">
             <img alt="robots" src={`https://robohash.org/${id}?size=200x200`}/>
             <div>
-                <h>{name}</h>
+                <h2>{name}</h2>
                 <p>{email}</p>
             </div>
         </div>
