34e5949aec3f1c7508cc969bc6a89cb162bb21b1
Han-Lynn-Aung <hanlynn.aung@cdsg.com.mm>
2024-11-03 12:19:58 +0630

    fixed folder structures and packages

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import './App.css';
+import Scroll from "../components/Scroll";
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => {
+                this.setState({robots: users})
+            });
+    }
+
+    onSearchChange = (e) => {
+        this.setState({searchField: e.target.value});
+    }
+
+    render() {
+        const {robots, searchField} = this.state;
+        const filterRobot = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+
+        return !robots.length ? <h1 className="tc pv7">Loading</h1> :
+            <div className="tc">
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filterRobot}/>
+                </Scroll>
+            </div>
+    }
+}
+
+export default App;
